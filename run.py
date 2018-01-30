d2960adfd9e84162aae8b606a561db6902728306
Hamed <hamedkalantari@icloud.com>
2018-01-30 20:18:16 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,42 +1,21 @@
 from keyword_extractor import KeywordExtractor
 from training_text_reader import TrainingTextReader
+from vectorizer import Vectorizer
+from vector_normalizer import VectorNormalizer
+from inverse_document_frequency import InverseDocumentFrequency
 
 
-reader = TrainingTextReader('HAM2-070107.xml')
-a = KeywordExtractor(reader.articles[10], 'useless.txt')
+def create_tf_idf(file_path):
+    reader = TrainingTextReader(file_path)
+    keywords = KeywordExtractor(reader.articles[10], 'useless.txt')
+    vector_index = Vectorizer(keywords.article_sents_tokened)
+    freq_mat = vector_index.frequencyMatrix
+    normalized_vector = VectorNormalizer(freq_mat)
+    norm_mat = normalized_vector.l2_norm_matrice
+    tf_idf = InverseDocumentFrequency(norm_mat)
+    return tf_idf.tf_idf_matrice
 
 
-print a.article_sents_tokened
+training_set_of_tf_idf = create_tf_idf('HAM2-070107.xml')
 
-class Vectorizer:
-    def __init__(self, article_sents_tokened):
-        self.article_sents_tokened = article_sents_tokened
-
-    def createVectorIndex(self):
-        self.vectorIndex = dict()
-        ctr = 0
-        for sent in self.article_sents_tokened:
-            for word in sent:
-                self.vectorIndex[ctr] = word
-                ctr += 1
-
-    def calculateFrequency(self, vocabulary_index, sentence_index):
-        ctr = 0
-        for word in self.article_sents_tokened[sentence_index]:
-            if word == self.vectorIndex[vocabulary_index]:
-                ctr += 1
-        return ctr
-
-    def frequencyDictionary(self):
-        self.frequency_dictionary = dict()
-        for sentence_ctr in range(len(self.article_sents_tokened)):
-            for word_index in self.vectorIndex.keys():
-                self.frequency_dictionary[(word_index, sentence_ctr)] = self.calculateFrequency(word_index, sentence_ctr)
-
-    def createFrequencyMatrix(self):
-        self.frequencyMatrix = []
-        for i in range(LEN_SENT):
-            temp = []
-            for j in range(LEN_WORD):
-                temp.append(self.frequency_dictionary[(j, i)])
-            self.frequencyMatrix.append(temp)
+print training_set_of_tf_idf
